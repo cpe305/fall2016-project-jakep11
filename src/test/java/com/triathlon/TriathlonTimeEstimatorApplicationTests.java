6cab1f6414167275f100383b2c8276ef024dc53b
Jake Pickett <j.pickett67@yahoo.com>
2016-10-28 00:36:31 -0700

    Fixed test file runner

--- src/test/java/com/triathlon/TriathlonTimeEstimatorApplicationTests.java
+++ src/test/java/com/triathlon/TriathlonTimeEstimatorApplicationTests.java
@@ -13,8 +13,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class TriathlonTimeEstimatorApplicationTests {
 
   @Test
