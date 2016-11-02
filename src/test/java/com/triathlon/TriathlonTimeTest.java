770d78879361df2e18b02fcfd5e1283f4bc7d421
Jake Pickett <j.pickett67@yahoo.com>
2016-11-02 12:07:24 -0700

    Fixed Hilary's test

--- src/test/java/com/triathlon/TriathlonTimeTest.java
+++ src/test/java/com/triathlon/TriathlonTimeTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import model.Time;
 import model.TriathlonTime;
+
 import org.junit.Test;
 
 public class TriathlonTimeTest {
@@ -15,11 +16,12 @@ public class TriathlonTimeTest {
     assertEquals(3909, triathtime1.getTotalTimeInSeconds());
   }
 
-
+  @Test
   public void testGetTotalTimeInSeconds2() {
-    TriathlonTime triathtime2 = new TriathlonTime(new Time(0, 0, 3), new Time(0, 0, 3),
+    TriathlonTime triathtime1 = new TriathlonTime(new Time(0, 0, 2), new Time(0, 0, 4),
         new Time(0, 0, 4), new Time(0, 5, 0), new Time(1, 0, 0));
-    assertEquals(3910, triathtime2.getTotalTimeInSeconds2());
+    assertEquals(3910, triathtime1.getTotalTimeInSeconds());
   }
+
 
 }
