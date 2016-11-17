7b3728d20dfa5f1e7dbb13f65aed9824141ebf7c
Jake Pickett <j.pickett67@yahoo.com>
2016-11-17 00:59:13 -0800

    commenting out unimplemented tests to fix Travis

--- src/test/java/com/triathlon/TriathlonDistanceTest.java
+++ src/test/java/com/triathlon/TriathlonDistanceTest.java
@@ -6,49 +6,49 @@ import org.junit.Test;
 
 public class TriathlonDistanceTest {
 
-  @Test
-  public void testTriathlonDistanceStandardDistance() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testTriathlonDistance() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testGetSwim() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testSetSwim() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testGetBike() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testSetBike() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testGetRun() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testSetRun() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testTriathlonDistanceDoubleDoubleDouble() {
-    fail("Not yet implemented");
-  }
+//  @Test
+//  public void testTriathlonDistanceStandardDistance() {
+//    fail("Not yet implemented");
+//  }
+//
+//  @Test
+//  public void testTriathlonDistance() {
+//    fail("Not yet implemented");
+//  }
+//
+//  @Test
+//  public void testGetSwim() {
+//    fail("Not yet implemented");
+//  }
+//
+//  @Test
+//  public void testSetSwim() {
+//    fail("Not yet implemented");
+//  }
+//
+//  @Test
+//  public void testGetBike() {
+//    fail("Not yet implemented");
+//  }
+//
+//  @Test
+//  public void testSetBike() {
+//    fail("Not yet implemented");
+//  }
+//
+//  @Test
+//  public void testGetRun() {
+//    fail("Not yet implemented");
+//  }
+//
+//  @Test
+//  public void testSetRun() {
+//    fail("Not yet implemented");
+//  }
+//
+//  @Test
+//  public void testTriathlonDistanceDoubleDoubleDouble() {
+//    fail("Not yet implemented");
+//  }
 
 }
