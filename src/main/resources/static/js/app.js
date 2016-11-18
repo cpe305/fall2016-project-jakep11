c085de800b02c834664198e9fedbecceef91925f
Jake Pickett <j.pickett67@yahoo.com>
2016-11-18 02:52:22 -0800

    Updated and created the views of website

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,72 +1,124 @@
+var app = angular.module('tri', [ 'ngRoute', 'ngMaterial', 'ngMessages' ]);
 
-var app = angular.module('tri', [ 'ngRoute' ]);
+app
+		.config(function($routeProvider, $httpProvider) {
 
-app.config(function($routeProvider, $httpProvider) {
+			$routeProvider.when('/', {
+				templateUrl : 'home.html',
+				controller : 'home'
+			}).when('/login', {
+				templateUrl : 'login.html',
+				controller : 'navigation'
+			}).when('/signup', {
+				templateUrl : 'signup.html',
+				controller : 'signup'
+			}).when('/triathlonList', {
+				templateUrl : 'triathlonList.html',
+				controller : 'triathlonList'
+			}).when('/addTriathlon', {
+				templateUrl : 'addTriathlon.html',
+				controller : 'addTriathlon'
+			}).when('/statistics', {
+				templateUrl : 'statistics.html',
+				controller : 'statistics'
+			}).otherwise('/');
 
-	$routeProvider.when('/', {
-		templateUrl : 'home.html',
-		controller : 'home'
-	})
-	.when('/login', {
-		templateUrl : 'login.html',
-		controller : 'navigation'
-	})
-	.otherwise('/');
+			$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 
-  $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-
-});
+		});
 
 app.controller('home', function($scope, $http) {
-  $http.get('/resource/').success(function(data) {
-    $scope.greeting = data;
-  })
+	$http.get('/resource/').success(function(data) {
+		$scope.greeting = data;
+	})
+});
+
+app.controller('triathlonList', function($scope, $http) {
+	//$scope.triathlons = ["a", "b", "c"];
+	$scope.triathlons = null;
+//	$http.get('/triathlons/').success(function(data) {
+//		$scope.triathlons = data;
+//	})
+	$http.get('/users/').success(function(data) {
+		$scope.users = data;
+		console.log(data);
+	})
+});
+
+app.controller('addTriathlon', function($scope, $http) {
+	$scope.name = "Name";
+	$scope.distance = "Olympic";
+	$scope.bikeElevation = 0;
+	$scope.runElevation = 0;
+	$scope.location = "San Luis Obispo";
+	$scope.date = new Date();
+});
+
+app.controller('statistics', function($scope, $http) {
+	$scope.triathlons = null;
+});
+
+app.controller('signup', function($scope, $http) {
+	$scope.error = false;
+	$scope.createUser = function() {
+		
+	}
+	
 });
 
 app.controller('navigation', function($rootScope, $scope, $http, $location) {
 
-	  var authenticate = function(credentials, callback) {
+	$scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+	
+	var authenticate = function(credentials, callback) {
 
-	    var headers = credentials ? {authorization : "Basic "
-	        + btoa(credentials.username + ":" + credentials.password)
-	    } : {};
+		var headers = credentials ? {
+			authorization : "Basic "
+					+ btoa(credentials.username + ":" + credentials.password)
+		} : {};
 
-	    $http.get('user', {headers : headers}).success(function(data) {
-	      if (data.name) {
-	        $rootScope.authenticated = true;
-	      } else {
-	        $rootScope.authenticated = false;
-	      }
-	      callback && callback();
-	    }).error(function() {
-	      $rootScope.authenticated = false;
-	      callback && callback();
-	    });
+		$http.get('user', {
+			headers : headers
+		}).success(function(data) {
+			if (data.name) {
+				$rootScope.authenticated = true;
+				$rootScope.name = data.name;
+				console.log(data.name);
+			} else {
+				$rootScope.authenticated = false;
+			}
+			callback && callback();
+		}).error(function() {
+			$rootScope.authenticated = false;
+			callback && callback();
+		});
 
-	  }
+	}
 
-	  authenticate();
-	  $scope.credentials = {};
-	  $scope.login = function() {
-	      authenticate($scope.credentials, function() {
-	        if ($rootScope.authenticated) {
-	          $location.path("/");
-	          $scope.error = false;
-	        } else {
-	          $location.path("/login");
-	          $scope.error = true;
-	        }
-	      });
-	  };
-	  
-	  $scope.logout = function() {
-		  $http.post('logout', {}).success(function() {
-		    $rootScope.authenticated = false;
-		    $location.path("/");
-		  }).error(function(data) {
-		    $rootScope.authenticated = false;
-		  });
-		}
-	  
+	authenticate();
+	$scope.credentials = {};
+	$scope.login = function() {
+		authenticate($scope.credentials, function() {
+			if ($rootScope.authenticated) {
+				$location.path("/");
+				$scope.error = false;
+			} else {
+				$location.path("/login");
+				$scope.error = true;
+			}
+		});
+	};
+
+	$scope.logout = function() {
+		$http.post('logout', {}).success(function() {
+			$rootScope.authenticated = false;
+			$rootScope.name = null;
+			$location.path("/");
+		}).error(function(data) {
+			$rootScope.authenticated = false;
+		});
+	}
+
 });
-
